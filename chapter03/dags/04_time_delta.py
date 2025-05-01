57c9bbb49a111e42dac91972a6cf78893caf5183
Daniel Vedia <danielvediaj@example.com>
2025-05-01 13:10:51 +0200

    Chap 3

--- chapter03/dags/04_time_delta.py
+++ chapter03/dags/04_time_delta.py
@@ -8,16 +8,21 @@ from airflow.operators.python import PythonOperator
 
 dag = DAG(
     dag_id="04_time_delta",
+    # schedule_interval="@daily",
     schedule_interval=dt.timedelta(days=3),
     start_date=dt.datetime(year=2019, month=1, day=1),
     end_date=dt.datetime(year=2019, month=1, day=5),
 )
 
+# Adding dates
 fetch_events = BashOperator(
     task_id="fetch_events",
     bash_command=(
         "mkdir -p /data/events && "
-        "curl -o /data/events.json http://events_api:5000/events"
+        "curl -o /data/events.json "
+        "http://events_api:5000/events?"
+        "start_date=2019-01-01&"
+        "end_date=2019-01-02"
     ),
     dag=dag,
 )
--- chapter03/dags/06_templated_query.py
+++ chapter03/dags/06_templated_query.py
@@ -8,27 +8,44 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
 dag = DAG(
-    dag_id="06_templated_query",
-    schedule_interval="@daily",
+    dag_id="04_time_delta",
+    # schedule_interval="@daily",
+    schedule_interval=dt.timedelta(days=3),
     start_date=dt.datetime(year=2019, month=1, day=1),
     end_date=dt.datetime(year=2019, month=1, day=5),
 )
 
+# Dates with jinja
+# fetch_events = BashOperator(
+#     task_id="fetch_events",
+#     bash_command=(
+#         "mkdir -p /data/events && "
+#         "curl -o /data/events.json "
+#         "http://events_api:5000/events?"
+#         "start_date={{execution_date.strftime('%Y-%m-%d')}}&"
+#         "end_date={{next_execution_date.strftime('%Y-%m-%d')}}"
+#     ),
+#     dag=dag,
+# )
+
+# # Adding ds in Jinja
 fetch_events = BashOperator(
     task_id="fetch_events",
     bash_command=(
         "mkdir -p /data/events && "
         "curl -o /data/events.json "
         "http://events_api:5000/events?"
-        "start_date={{execution_date.strftime('%Y-%m-%d')}}&"
-        "end_date={{next_execution_date.strftime('%Y-%m-%d')}}"
+        "start_date={{ds}}&"
+        "end_date={{next_ds}}"
     ),
     dag=dag,
 )
 
 
-def _calculate_stats(input_path, output_path):
+def _calculate_stats(**context):
     """Calculates event statistics."""
+    input_path = context["templates_dict"]["input_path"]
+    output_path = context["templates_dict"]["output_path"]
 
     events = pd.read_json(input_path)
     stats = events.groupby(["date", "user"]).size().reset_index()
@@ -40,8 +57,24 @@ def _calculate_stats(input_path, output_path):
 calculate_stats = PythonOperator(
     task_id="calculate_stats",
     python_callable=_calculate_stats,
-    op_kwargs={"input_path": "/data/events.json", "output_path": "/data/stats.csv"},
+    templates_dict={
+        "input_path": "/data/events/{{ds}}.json",
+        "output_path": "/data/stats/{{ds}}.csv",
+    },
     dag=dag,
 )
 
-fetch_events >> calculate_stats
+def _send_stats(email, **context):
+    stats = pd.read_csv(context["templates_dict"]["stats_path"])
+    # email_stats(stats, email=email)
+
+
+send_stats = PythonOperator(
+    task_id="send_stats",
+    python_callable=_send_stats,
+    op_kwargs={"email": "user@example.com"},
+    templates_dict={"stats_path": "/data/stats/{{ds}}.csv"},
+    dag=dag,
+)
+
+fetch_events >> calculate_stats >> send_stats
